d5102ef3c9328d09a66fbf35ebacea2e876d3595
Prajjwal Veer Basnet <prajjwalveerbasnet10@gmail.com>
2023-10-05 00:20:23 +0545

    Created Simple Login and Registration Page.

--- src/org/cis/week2/LoginPage.java
+++ src/org/cis/week2/LoginPage.java
@@ -3,75 +3,193 @@
  */
 package org.cis.week2;
 
-import java.awt.Frame;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 /**
  * 
  */
-public class LoginPage extends JFrame{
+public class LoginPage extends JFrame implements ActionListener, ChangeListener{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	JTextField uNameTextField;
-	JTextField passwordField;
+	
+	JFrame frame;
+	
+	// Labels Declaration 
+	
+	JLabel title;
 	JLabel userName;
 	JLabel password;
+	
+	// TextField Declaration
+	
+	JTextField uNameTextField;
+	JPasswordField passwordField;  // PasswordField for passwords
+	
+	// Button Declaration 
+	
 	JButton login;
-	JButton cancel;
+	JButton clear;
+	JButton register;
 	
-public LoginPage() {
+	// Check Box Field for Passwords
 	
-	setDefaultCloseOperation(EXIT_ON_CLOSE);
+	JCheckBox tick;
 	
-	uNameTextField = new JTextField();
-	uNameTextField.setBounds(200, 50, 150, 20);
+	public LoginPage() {
 	
-	passwordField = new JTextField();
-	passwordField.setBounds(200, 80, 150, 20);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	
-	userName = new JLabel("Username");
-	userName.setBounds(50, 50, 250, 20);
+		// Labels 
 	
-	password = new JLabel("Password");
-	password.setBounds(50, 80, 250, 20);
+		userName = new JLabel("Username");
+		userName.setBounds(50, 50, 250, 20);
 	
-	login = new JButton("Login");
-	login.setBounds(70, 110, 95, 30);
+		password = new JLabel("Password");
+		password.setBounds(50, 80, 250, 20);
 	
-	cancel = new JButton("Cancel");
-	cancel.setBounds(200, 110, 95, 30);
+		title = new JLabel("Login Page");
+		title.setBounds(150, 20, 250,20);
 	
-	add(uNameTextField);
-	add(passwordField);
-	add(userName);
-	add(password);
-	add(login);
-	add(cancel);
+		// TextFields
 	
+		uNameTextField = new JTextField();
+		uNameTextField.setBounds(200, 50, 150, 20);
 	
+		passwordField = new JPasswordField();
+		passwordField.setBounds(200, 80, 150, 20);
+		passwordField.setEchoChar('*');
 	
-	setSize(400, 400);
-	setLayout(null);
-	setVisible(true);
+		// Buttons
 	
-}
+		login = new JButton("Login");
+		login.setBounds(70, 140, 95, 30);
 	
+		clear = new JButton("Clear");
+		clear.setBounds(200, 140, 95, 30);
+		
+		register = new JButton("Register");
+		register.setBounds(140, 190, 95, 30);
+		
+		// Check Box 
+		
+		tick = new JCheckBox("Show Password");
+		tick.setBounds(200, 100, 150, 20);
+		
 	
+		// Action Listeners 
+		
+		register.addActionListener(this);
+		login.addActionListener(this);
+		clear.addActionListener(this);
+		
+		clear.addChangeListener(this);
+		
+		tick.addActionListener(this);
+		
+		// Adding it to the frame
+	
+		add(userName);
+		add(password);
+		add(title);
+	
+		add(uNameTextField);
+		add(passwordField);
+	
+		add(login);
+		add(clear);
+		add(register);
+		
+		add(tick);
+	
+		// Frame Settings
+	
+		setSize(400, 400);
+		setLayout(null);
+		setVisible(true);
+	
+	}
+	
+	private void LoginUser() {
+        String username = uNameTextField.getText();
+        String password = passwordField.getText();
 
-	/**
-	 * @param args
-	 */
+        if(username.isEmpty() || password.isEmpty()) {
+        	
+            JOptionPane.showMessageDialog(frame, "Please fill in all the fields.", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        // TODO: Add your user registration logic here (e.g., save to database)
+
+        JOptionPane.showMessageDialog(frame, "Login successfully!", "Success", JOptionPane.INFORMATION_MESSAGE);
+        // System.out.println(password);
+    }
+	
+	private void clearTexts() {
+		uNameTextField.setText(null);
+		passwordField.setText(null);
+	}
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new LoginPage();
+			// TODO Auto-generated method stub
+			new LoginPage();
 
+	}
+	
+	
+	
+
+	// Action performed for register button
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() == register) {
+			new RegistrationPage();
+			dispose(); 
+		}
+		if(e.getSource() == login) {
+			LoginUser();
+		}
+		if(e.getSource() == clear) {
+			clearTexts();
+		}
+		if(tick.isSelected()){
+			passwordField.setEchoChar((char)0);
+		}
+		else {
+			passwordField.setEchoChar('*');
+		}
+		
+		
+			
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		// TODO Auto-generated method stub
+		
+		
 	}
 
 }
---
+++ src/org/cis/week2/RegistrationPage.java
@@ -0,0 +1,200 @@
+/**
+ * 
+ */
+package org.cis.week2;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+/**
+ * 
+ */
+public class RegistrationPage extends JFrame implements ActionListener{
+	
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+		JFrame frame;
+	
+		// Labels Declaration 
+	
+		JLabel title;
+		JLabel name;
+		JLabel address;
+		JLabel number;
+		JLabel username;
+		JLabel password;
+		
+		// TextField Declaration
+		
+		JTextField nameField;
+		JTextField addressField;
+		JTextField numberField;
+		JTextField UserNameField;
+		JPasswordField passwordField;
+		
+		// Button Declaration 
+		
+		JButton register;
+		JButton clear;
+		JButton goBack;
+		
+		// Check Box
+		
+		JCheckBox check;
+		
+		public RegistrationPage() {
+			
+			setDefaultCloseOperation(EXIT_ON_CLOSE);
+			
+			// Labels 
+		
+			name = new JLabel("Name");
+			name.setBounds(50, 50, 250, 20);
+			
+			address = new JLabel("Address");
+			address.setBounds(50, 80, 250, 20);
+			
+			number = new JLabel("Phone Number");
+			number.setBounds(50, 110, 250, 20);
+			
+			username = new JLabel("Username");
+			username.setBounds(50, 140, 250, 20);
+		
+			password = new JLabel("Password");
+			password.setBounds(50, 170, 250, 20);
+		
+			title = new JLabel("Registration Page");
+			title.setBounds(150, 20, 250, 20);
+		
+			// TextFields
+		
+			nameField = new JTextField();
+			nameField.setBounds(200, 50, 150, 20);
+			
+			addressField = new JTextField();
+			addressField.setBounds(200, 80, 150, 20);
+			
+			numberField = new JTextField();
+			numberField.setBounds(200, 110, 150, 20);
+			
+			UserNameField = new JTextField();
+			UserNameField.setBounds(200, 140, 150, 20);
+		
+			passwordField = new JPasswordField();
+			passwordField.setBounds(200, 170, 150, 20);
+			passwordField.setEchoChar('*');
+		
+			// Buttons
+		
+			register = new JButton("Register");
+			register.setBounds(70, 220, 95, 30);
+		
+			clear = new JButton("Clear");
+			clear.setBounds(200, 220, 95, 30);
+			
+			goBack = new JButton("Go Back");
+			goBack.setBounds(135, 260, 95, 30);
+			
+			// Check Box
+			
+			check = new JCheckBox("Show Password");
+			check.setBounds(200, 190, 150, 20);
+			
+		
+			
+			// Adding it to the frame
+		
+			add(name);
+			add(address);
+			add(number);
+			add(username);
+			add(password);
+			add(title);
+		
+			add(nameField);
+			add(addressField);
+			add(numberField);
+			add(UserNameField);
+			add(passwordField);
+		
+			add(register);
+			add(clear);
+			add(goBack);
+			add(check);
+			
+			register.addActionListener(this);
+			clear.addActionListener(this);
+			goBack.addActionListener(this);
+			check.addActionListener(this);
+		
+			// Frame Settings
+		
+			setSize(400, 400);
+			setLayout(null);
+			setVisible(true);
+		}
+		
+		private void RegisterUser() {
+			String name = nameField.getText();
+			String address = addressField.getText();
+			String number = numberField.getText();
+	        String username = UserNameField.getText();
+	        String password = passwordField.getText();
+
+	        if(name.isEmpty() || address.isEmpty() || number.isEmpty() || username.isEmpty() || password.isEmpty()) {
+	        	
+	            JOptionPane.showMessageDialog(frame, "Please fill in all the fields.", "Error", JOptionPane.ERROR_MESSAGE);
+	            return;
+	        }
+
+	        // TODO: Add your user registration logic here (e.g., save to database)
+
+	        JOptionPane.showMessageDialog(frame, "Registration successfully done!", "Success", JOptionPane.INFORMATION_MESSAGE);
+	        new LoginPage();
+	        dispose();
+	    }
+		
+		private void ClearTexts() {
+			nameField.setText(null);
+			addressField.setText(null);
+			numberField.setText(null);
+			UserNameField.setText(null);
+			passwordField.setText(null);
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			if(e.getSource() == register) {
+				RegisterUser();
+			}
+			if(e.getSource() == clear) {
+				ClearTexts();
+			}
+			if(e.getSource() == goBack) {
+				new LoginPage();
+				dispose();
+			}
+			if(check.isSelected()){
+				passwordField.setEchoChar((char)0);
+			}
+			else {
+				passwordField.setEchoChar('*');
+			}
+			
+		}
+
+}
